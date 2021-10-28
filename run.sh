b45878bf75459c5d3f271e2de80ea28f5ea18017
Saranraj Nambusubramaniyan <saran_nns@hotmail.com>
2021-10-28 14:47:01 +0200

    fix absolute paths at run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker build -f Dockerfile -t huggingtext:v0.1.0 .
 
 docker run -p 7000:5000 \
-           -v home/saran/huggingtext/:/root/huggingtext \
+           -v /home/saran/huggingtext/:/root/huggingtext \
            -ti huggingtext:v0.1.0 /bin/bash \
            -c "cd /root/huggingtext/ \
                && source activate huggingtext \ 
